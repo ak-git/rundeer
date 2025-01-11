1426b2c916e188194cd1e37a7ed8206955310383
Alexander Kobelev <ak.mail.ru@gmail.com>
2025-01-11 23:11:01 +0300

    Add bulma layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,61 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import cyberconstructor from './assets/cyberconstructor.png'
+    import cyberchief from './assets/cyberchief.png'
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <section class="hero is-small">
+        <div class="hero-head">
+            <nav class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <div class="navbar-item">
+                            <img src="favicon.svg" alt="Logo"/>
+                        </div>
+                        <div class="navbar-item">
+                            <p class="title">Цифровой тест на Здоровье</p>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div class="hero-body">
+            <div class="container columns has-text-centered">
+                <div class="column">
+                    <div>
+                        <article class="panel is-warning">
+                            <p class="panel-heading">
+                                <a class="tag is-rounded is-large is-hoverable"
+                                   href="http://www.cyberconstructor.ru.s3-website-us-east-1.amazonaws.com">
+                                    КиберКонструктор
+                                </a>
+                            </p>
+                            <a href="http://www.cyberconstructor.ru.s3-website-us-east-1.amazonaws.com">
+                                <figure class="image">
+                                    <img src="{cyberconstructor}" alt="cyberconstructor"/>
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+                <div class="column">
+                    <div>
+                        <article class="panel is-dark">
+                            <p class="panel-heading">
+                                <a class="tag is-rounded is-large is-hoverable"
+                                   href="http://www.cyberchief.ru.s3-website-us-east-1.amazonaws.com">
+                                    КиберГлавный
+                                </a>
+                            </p>
+                            <a href="http://www.cyberchief.ru.s3-website-us-east-1.amazonaws.com">
+                                <figure class="image is-fullwidth">
+                                    <img src="{cyberchief}" alt="cyberchief"/>
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
