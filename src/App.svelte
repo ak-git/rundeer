33d032038fa050b6577450bb6b4a98ecffe9ec7a
Alexander Kobelev <ak.mail.ru@gmail.com>
2025-01-12 14:30:03 +0300

    Изменение цветов

--- src/App.svelte
+++ src/App.svelte
@@ -8,12 +8,14 @@
         <div class="hero-head">
             <div class="container box has-text-centered media">
                 <div class="media-left">
-                    <figure class="image is-48x48">
+                    <figure class="image is-64x64">
                         <img src="favicon.svg" alt="Logo"/>
                     </figure>
                 </div>
                 <div class="media-content">
-                    <p class="title">Цифровой тест на Здоровье</p>
+                    <p class="title button is-info">
+                        Цифровой тест на Здоровье
+                    </p>
                 </div>
             </div>
         </div>
